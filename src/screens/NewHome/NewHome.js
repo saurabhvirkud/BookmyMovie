53a1c4e08cdc962f85ff12f2af92e41e630929cb
saurabhvirkud <harsha@carok.in>
2021-04-20 00:09:35 +0530

    username changed

---
+++ src/screens/NewHome/NewHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookShow from '../bookshow/BookShow';
+import './newhome.css';
+const NewHome =() =>{
+    return(
+        <div className='div-newhome'>
+            <BookShow/>
+        </div>
+    )
+}
+export default NewHome;
